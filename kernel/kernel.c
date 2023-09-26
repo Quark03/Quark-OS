ad63845b7bce12847cb9710ef89eec88b9ddd2f0
Antonio Cinca Festas <quark03.dev@gmail.com>
2023-09-26 22:25:05 +0100

    Refactor a bit the kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,15 @@
+//
+// Kernel.c
+//
+
+#include "kernel.h"
+
+/**
+ * Kernel starting point
+ */
 void _start()
 {
-    // Create a pointer to a char, and point it to the first text cell of
-    // video memory (i.e. the top-left of the screen)
-    char *video_memory = (char *)0xb8000;
-    // At the address pointed to by video_memory, store the character ’X’ // (i.e. display ’X’ in the top-left of the screen).
+    char *video_memory = VIDEO_ADDRESS;
+
     *video_memory = 'X';
 }
