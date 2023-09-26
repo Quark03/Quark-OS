538d488f8966e2de3613f1aae134ec22fcb6375e
Antonio Cinca <quark03.dev@gmail.com>
2023-09-26 15:03:25 +0100

    Preparing to execute kernel from bootloader

--- Makefile
+++ Makefile
@@ -11,23 +11,36 @@ TCOLOR_PURPLE="\033[35m"
 TCOLOR_CYAN="\033[36m"
 TCOLOR_WHITE="\033[37m"
 
-BOOTLOADER_DIR=bootloader
-BOOTLOADER_SRC=$(BOOTLOADER_DIR)/bootloader.asm
+BOOTLOADER_SRC=bootloader/bootloader.asm
+KERNEL_SRC=kernel/kernel.c
 
-all:	bootloader
+all:	group
 	@echo $(TCOLOR_PURPLE)Starting os emulation ...$(TCOLOR_RESET)
 	@qemu-system-x86_64 -drive format=raw,file=$(NAME)
 
+group: bootloader kernel
+	@echo $(TCOLOR_CYAN)Building OS image ...$(TCOLOR_RESET)
+	@cat bootloader.bin kernel.bin > $(NAME)
+	@echo $(TCOLOR_GREEN)OS Image built successfully$(TCOLOR_RESET)
 
 bootloader: $(BOOTLOADER_SRC)
 	@clear
 	@echo $(TCOLOR_CYAN)Assembling bootloader ...$(TCOLOR_RESET)
-	@nasm $(BOOTLOADER_SRC) -f bin -o $(NAME)
+	@nasm $(BOOTLOADER_SRC) -f bin -o bootloader.bin
 	@echo $(TCOLOR_GREEN)Bootloader assembled$(TCOLOR_RESET)
+
+
+# ld -o kernel.bin -Ttext 0x1000 kernel.o --oformat binary
+# @arch -x86_64 objcopy -O binary -j .text kernel-ld kernel.bin
+kernel:
+	@echo $(TCOLOR_CYAN)Compiling kernel ...$(TCOLOR_RESET)
+	@gcc -arch x86_64 -ffreestanding -c $(KERNEL_SRC) -o kernel.o
+	@ld -arch x86_64 -o kernel-ld -e 0x1000 kernel.o
+	@echo $(TCOLOR_GREEN)Kernel compiled successfully$(TCOLOR_RESET)
 
 clean:
 	rm -f $(NAME)
 
 re: clean all
 
-.PHONY: bootloader clear re
+.PHONY: bootloader kernel group clean re
--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -3,11 +3,17 @@
 ;
 [org 0x7c00]    ; Tell the asembler where the code will be loaded
 
+KERNEL_OFFSET equ 0x1000    ; This is the memory offset to which we will load our kernel
+
+mov [BOOT_DRIVE], dl ; BIOS stores our boot drive in DL, so it’s ; best to remember this for later.
+
 mov bp, 0x9000  ; Set the stack
 mov sp, bp
 
 mov bx, MSG_REAL_MODE
 call print_string
+
+call load_kernel
 
 call switch_to_pm   ; Note that we never return from here.
 
@@ -16,10 +22,27 @@ jmp $
 ;
 ; Includes
 ;
-%include "./bootloader/print_string.asm"
-%include "./bootloader/gdt.asm"
-%include "./bootloader/print_string_pm.asm"
-%include "./bootloader/switch_to_pm.asm"
+%include "./bootloader/print/print_string.asm"
+%include "./bootloader/disk/disk_load.asm"
+%include "./bootloader/pm/gdt.asm"
+%include "./bootloader/pm/print_string_pm.asm"
+%include "./bootloader/pm/switch_to_pm.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL     ;  Print a message to say we are loading the kernel
+    call print_string
+
+    ; Set -up parameters for our disk_load routine , so
+    ; that we load the first 15 sectors (excluding
+    ; the boot sector) from the boot disk (i.e. our
+    ; kernel code) to address KERNEL_OFFSET
+    mov bx, KERNEL_OFFSET
+    mov dh , 15
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+
+    ret
 
 [bits 32]
 ; This is where we arrive after switching to and initialising protected mode
@@ -27,22 +50,20 @@ jmp $
 BEGIN_PM:
     mov ebx , MSG_PROT_MODE
     call print_string_pm        ; Use our 32-bit print routine. 
+
+    call KERNEL_OFFSET          ; Now jump to the address of our loaded kernel code , assume the brace position , 
+                                ; and cross your fingers. Here we go!
+
     jmp $                       ; Hang.
 
 ; Global variables
-MSG_REAL_MODE: db 'Started in 16-bit Real Mode', 0
-MSG_PROT_MODE: db 'Successfully landed in 32-bit Protected Mode', 0
+BOOT_DRIVE db 0
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE  db "Successfully landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory.", 0
 
 ;
 ; Padding and magic BIOS number.
 ;
 times 510-($-$$) db 0
 dw 0xaa55
-
-
-; We know that BIOS will load only the first 512-byte sector from the disk, 
-; so if we purposely add a few more sectors to our code by repeating some
-; familiar numbers, we can prove to ourselfs that we actually loaded those 
-; additional two sectors from the disk we booted from.
-; times 256 dw 0xdada 
-; times 256 dw 0xface
